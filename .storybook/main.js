ad88d7ce0132024f8580c0130b1914328e132e05
Ryan Haskell-Glatz <ryan.nhg@gmail.com>
2023-07-25 17:50:07 -0500

    both storybook:dev and storybook:build work with minimal dev dependencies

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,12 @@
 import ElmVitePlugin from 'vite-plugin-elm'
 
 export default {
-  "stories": [
-    "../src/Stories/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: [
+    "../src/Stories/**/*.stories.js"
   ],
-  "addons": [
-    "@storybook/addon-essentials"
-  ],
-  "framework": {
-    "name": "@storybook/html-vite",
-    "options": {}
+  framework: {
+    name: "@storybook/html-vite",
+    options: {}
   },
   async viteFinal(config) {
     return {
@@ -18,11 +15,9 @@ export default {
         ...config.plugins,
         ElmVitePlugin({ debug: false, optimize: false })
       ],
-      define: {
-        ...config.define,
-        // Prevents "global is not defined" error
-        global: "window",
-      },
-    };
+      define: (config.build)
+        ? config.define
+        : { ...config.define, global: "window" }
+    }
   }
 };
